1b7b7fa6c7f8f8161d788b8729507bb4ed479944
Sourov <sourovdas730@gmail.com>
2024-07-17 13:58:54 +0600

    add : api for aunthentication

--- index.js
+++ index.js
@@ -28,14 +28,109 @@ async function run() {
         await client.connect();
         const database = client.db("paymate");
         const userCollection = database.collection('users');
-        
+        const agentCollection = database.collection('agents');
 
-        app.post('/reg-user', async (req, res) => {
-            const {password} = req.body;
-            console.log(password);
+        app.post('/log-user', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const { userIdentity, password } = user;
+
+            const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const checkMail = emailRegEx.test(userIdentity);
+
+            console.log(checkMail);
+            if (checkMail) {
+                const query = { email: userIdentity };
+                console.log('email query -', query);
+                const result = await userCollection.findOne(query);
+                console.log(result);
+                if (result) {
+                    bycrypt.compare(password, result?.password, (err, response) => {
+                        console.log('yes', response);
+                        if (response === true) {
+                            res.send({ isCorrect: true });
+                        }
+                        else {
+                            res.send({ isCorrect: false })
+                        }
+
+                    })
+                }
+                else{
+                    res.send({message : 'This email is not registered.'})
+                }
+
+            }
+            else {
+                const query = { phone: userIdentity };
+                console.log(query);
+                const result = await userCollection.findOne(query);
+                console.log(result);
+                if (result) {
+                    bycrypt.compare(password, result?.password, (err, response) => {
+                        console.log('yes', response);
+                        if (response === true) {
+                            res.send({ isCorrect: true });
+                        }
+                        else {
+                            res.send({ isCorrect: false })
+                        }
+
+                    })
+                }
+                else{
+                    res.send({message : 'This Phone Number is not registered.'})
+                }
+            }
+
         })
 
-        
+        app.post('/reg-user', async (req, res) => {
+            const { password, role, name, phone, email } = req.body;
+            // console.log(password, role);
+            const user = { name, phone, email, role };
+
+            const phoneQuery = {phone : phone};
+            const emailQuery = {email : email};
+
+            const phoneResult = await userCollection.findOne(phoneQuery);
+            if(phoneResult){
+                res.send({message : 'This phone number already exist'});
+                return;
+            }
+
+            const emailResult = await userCollection.findOne(emailQuery);
+            if(emailResult){
+                res.send({message : 'This email already exist'});
+                return;
+            }
+
+            bycrypt.genSalt(10, (err, salt) => {
+                bycrypt.hash(password, salt, async (err, hashPass) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(hashPass);
+                    user.password = hashPass;
+                    console.log(user);
+                    const result = await userCollection.insertOne(user);
+                    console.log(result);
+                    res.send(result);
+                    // if(role === 'user'){
+                    //     const result = await userCollection.insertOne(user);
+                    //     console.log(result);
+                    //     res.send(result);
+                    // }
+                    // if(role === 'agent'){
+                    //     const result = await agentCollection.insertOne(user);
+                    //     res.send(result);
+                    // }
+                })
+            })
+        })
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
